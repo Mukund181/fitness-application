734ae6818bc16dd071c6c192497f7918509a8a23
Mukund181 <mukund.fegade@gmail.com>
2026-03-21 17:00:21 +0545

    Fix CORS policy for live frontend

--- server/server.js
+++ server/server.js
@@ -10,7 +10,14 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ 
+  origin: [
+    'http://localhost:5173', 
+    'https://fitness-application-ten.vercel.app',
+    'https://fitness-application-u3qv.onrender.com'
+  ], 
+  credentials: true 
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
